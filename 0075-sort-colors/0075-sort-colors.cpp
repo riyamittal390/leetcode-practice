7cbcfa462d2f28988fc5d455913347c0d958f2fe
riyamittal390 <riyamittal390@gmail.com>
2026-07-24 21:30:29 +0530

    Time: 0 ms (100%), Space: 11.6 MB (49.39%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,31 +1,69 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int n = nums.size();
-        int cnt0 = 0;
-        int cnt1 = 0;
-        int cnt2 = 0;
 
-        for (int i = 0 ; i < n ; i++) {
-            if (nums[i] == 0) {
-                cnt0++;
+        // Bruteforce
+
+        // sort(nums.begin(), nums.end());
+
+
+
+
+
+
+        // Better
+
+        // int n = nums.size();
+        // int cnt0 = 0;
+        // int cnt1 = 0;
+        // int cnt2 = 0;
+
+        // for (int i = 0 ; i < n ; i++) {
+        //     if (nums[i] == 0) {
+        //         cnt0++;
+        //     }
+        //     else if (nums[i] == 1) {
+        //         cnt1++;
+        //     }
+        //     else {
+        //         cnt2++;
+        //     }
+        // }
+
+        // for (int i = 0 ; i < cnt0 ; i++) {
+        //     nums[i] = 0;
+        // }
+        // for (int i = cnt0 ; i < cnt0 + cnt1 ; i++) {
+        //     nums[i] = 1;
+        // }
+        // for (int i = cnt0 + cnt1 ; i < n ; i++) {
+        //     nums[i] = 2;
+        // }
+
+
+
+
+
+
+
+        // Optimal
+
+        int n = nums.size();
+        int low = 0, mid = 0, high = n-1;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap (nums[low], nums[mid]);
+                low++;
+                mid++;
             }
-            else if (nums[i] == 1) {
-                cnt1++;
+            else if (nums[mid] == 1) {
+                mid++;
             }
             else {
-                cnt2++;
+                swap(nums[high], nums[mid]);
+                high--;
             }
-        }
-
-        for (int i = 0 ; i < cnt0 ; i++) {
-            nums[i] = 0;
-        }
-        for (int i = cnt0 ; i < cnt0 + cnt1 ; i++) {
-            nums[i] = 1;
-        }
-        for (int i = cnt0 + cnt1 ; i < n ; i++) {
-            nums[i] = 2;
         }
     }
 };
