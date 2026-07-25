18c51c5333c0adddacce8eb55307bef4aad43180
riyamittal390 <riyamittal390@gmail.com>
2026-07-25 20:49:00 +0530

    Time: 1 ms (34.95%), Space: 28.3 MB (37.42%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+
+        // Bruteforce
+
+        // int n = nums.size();
+        // for (int i = 0 ; i < n ; i++) {
+        //     int cnt = 0;
+        //     for (int j = 0 ; j < n ; j++) {
+        //         if (nums[j] == nums[i]) {
+        //             cnt++;
+        //         }
+        //     }
+        //     if (cnt > n/2) {
+        //         return nums[i];
+        //     }
+        // }
+        // return -1;
+
+
+
+
+
+        // Better
+
+        int n = nums.size();
+        int freq = 1;
+        int ans = nums[0];
+        sort (nums.begin(), nums.end());
+
+        for (int i = 1 ; i < n ; i++) {
+            if (nums[i] == nums[i-1]) {
+                freq++;
+            }
+            else {
+                freq = 1;
+                ans = nums[i];
+            }
+
+            if (freq > n/2) {
+                return nums[i];
+            }
+        }
+
+        return nums[0];
+    }
+};
