6b816d143d6581c99754b04da7f87c9441642b76
riyamittal390 <riyamittal390@gmail.com>
2026-07-25 21:28:38 +0530

    Time: 0 ms (100%), Space: 28.2 MB (37.42%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -24,25 +24,52 @@ public:
 
         // Better
 
-        int n = nums.size();
-        int freq = 1;
-        int ans = nums[0];
-        sort (nums.begin(), nums.end());
+        // int n = nums.size();
+        // int freq = 1;
+        // int ans = nums[0];
+        // sort (nums.begin(), nums.end());
 
-        for (int i = 1 ; i < n ; i++) {
-            if (nums[i] == nums[i-1]) {
+        // for (int i = 1 ; i < n ; i++) {
+        //     if (nums[i] == nums[i-1]) {
+        //         freq++;
+        //     }
+        //     else {
+        //         freq = 1;
+        //         ans = nums[i];
+        //     }
+
+        //     if (freq > n/2) {
+        //         return nums[i];
+        //     }
+        // }
+
+        // return nums[0];
+
+
+
+
+
+
+
+
+        // Optimal
+
+        int n = nums.size();
+        int freq = 0;
+        int ans = 0;
+
+        for (int i = 0 ; i < n ; i++) {
+            if (freq == 0) {
+                ans = nums[i];
+            }
+            if (ans == nums[i]) {
                 freq++;
             }
             else {
-                freq = 1;
-                ans = nums[i];
-            }
-
-            if (freq > n/2) {
-                return nums[i];
+                freq--;
             }
         }
 
-        return nums[0];
+        return ans;
     }
 };
