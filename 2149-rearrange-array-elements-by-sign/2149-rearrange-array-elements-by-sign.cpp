35d671202faf9f3432fcfba689324453ccd89dce
riyamittal390 <riyamittal390@gmail.com>
2026-07-28 22:34:24 +0530

    Time: 10 ms (31.34%), Space: 128.4 MB (54.49%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,24 +1,54 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        int n = nums.size();
 
-        vector<int> positive;
-        vector<int> negative;
+        // Bruteforce
+
+        // int n = nums.size();
+
+        // vector<int> positive;
+        // vector<int> negative;
+
+        // for (int i = 0 ; i < n ; i++) {
+        //     if (nums[i] > 0) {
+        //         positive.push_back(nums[i]);
+        //     }
+        //     else {
+        //         negative.push_back(nums[i]);
+        //     }
+        // }
+
+        // for (int i = 0 ; i < n/2 ; i++) {
+        //     nums[2*i] = positive[i];
+        //     nums[2*i+1] = negative[i];
+        // }
+        // return nums;
+
+
+
+
+
+
+
+
+        // Optimal
+
+        int n = nums.size();
+        vector<int> ans(n);
+        int positiveIndex = 0;
+        int negativeIndex = 1;
 
         for (int i = 0 ; i < n ; i++) {
             if (nums[i] > 0) {
-                positive.push_back(nums[i]);
+                ans[positiveIndex] = nums[i];
+                positiveIndex += 2;
             }
             else {
-                negative.push_back(nums[i]);
+                ans[negativeIndex] = nums[i];
+                negativeIndex += 2;
             }
         }
 
-        for (int i = 0 ; i < n/2 ; i++) {
-            nums[2*i] = positive[i];
-            nums[2*i+1] = negative[i];
-        }
-        return nums;
+        return ans;
     }
 };
