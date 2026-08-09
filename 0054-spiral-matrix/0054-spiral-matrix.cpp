5a6ad5598a970912e5edf13f6af9cfa32530a069
riyamittal390 <riyamittal390@gmail.com>
2026-08-09 15:06:11 +0530

    Time: 0 ms (100%), Space: 9.3 MB (57.46%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,40 +1,94 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
+        // int n = matrix.size();
+        // int m = matrix[0].size();
 
-        int left = 0, right = m-1;
-        int top = 0, bottom = n-1;
+        // int left = 0, right = m-1;
+        // int top = 0, bottom = n-1;
+
+        // vector<int> ans;
+
+        // while (top <= bottom && left <= right) {
+        //     for (int i = left ; i <= right ; i++) {
+        //     ans.push_back(matrix[top][i]);
+        //     }
+        //     top++;
+
+        //     for (int i = top ; i <= bottom ; i++) {
+        //         ans.push_back(matrix[i][right]);
+        //     }
+        //     right--;
+
+        //     if (top <= bottom) {
+        //         for (int i = right ; i >= left ; i--) {
+        //             ans.push_back(matrix[bottom][i]);
+        //         }
+        //         bottom--;
+        //     }
+
+        //     if (left <= right) {
+        //         for (int i = bottom ; i >= top ; i--) {
+        //             ans.push_back(matrix[i][left]);
+        //         }
+        //         left++;
+        //     }
+        // }
+
+        // return ans;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int starting_row = 0, ending_row = m-1;
+        int starting_col = 0, ending_col = n-1;
 
         vector<int> ans;
 
-        while (top <= bottom && left <= right) {
-            for (int i = left ; i <= right ; i++) {
-            ans.push_back(matrix[top][i]);
+        while (starting_row <= ending_row && starting_col <= ending_col) {
+            for (int i = starting_col ; i <= ending_col ; i++) {
+                ans.push_back(matrix[starting_row][i]);
             }
-            top++;
 
-            for (int i = top ; i <= bottom ; i++) {
-                ans.push_back(matrix[i][right]);
+            for (int j = starting_row+1 ; j <= ending_row ; j++) {
+                ans.push_back(matrix[j][ending_col]);
             }
-            right--;
 
-            if (top <= bottom) {
-                for (int i = right ; i >= left ; i--) {
-                    ans.push_back(matrix[bottom][i]);
+            for (int i = ending_col-1 ; i >= starting_col ; i--) {
+                if (starting_row == ending_row) {
+                    break;
                 }
-                bottom--;
+                ans.push_back(matrix[ending_row][i]); 
             }
-
-            if (left <= right) {
-                for (int i = bottom ; i >= top ; i--) {
-                    ans.push_back(matrix[i][left]);
+            for (int j = ending_row-1 ; j >= starting_row+1 ; j--) {
+                if (starting_col == ending_col) {
+                    break;
                 }
-                left++;
+                ans.push_back(matrix[j][starting_col]);
             }
-        }
 
-        return ans;
+            starting_row++; ending_row--;
+            starting_col++; ending_col--;
+            }
+
+            return ans;
     }
 };
